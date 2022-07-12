8a140fd925f43004f03418b2d152dffd779dc07a
rizzoirfan <rizzo.irfan@knowledgetouch.com>
2022-07-12 19:20:33 +0700

    starter project

---
+++ src/main/java/com/gimmenow/deliveryrestapi/DeliveryRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.gimmenow.deliveryrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gimmenow/deliveryrestapi/DeliveryRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gimmenow.deliveryrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
