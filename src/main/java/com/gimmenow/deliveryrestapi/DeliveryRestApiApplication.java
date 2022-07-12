f8898e82ff1757bef7f5e0af8ebb028f39627fab
rizzoirfan <rizzo.irfan@knowledgetouch.com>
2022-07-12 20:21:34 +0700

    added swagger

--- src/main/java/com/gimmenow/deliveryrestapi/DeliveryRestApiApplication.java
+++ src/main/java/com/gimmenow/deliveryrestapi/DeliveryRestApiApplication.java
@@ -3,11 +3,15 @@ package com.gimmenow.deliveryrestapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *
+ * @author Rizzo
+ */
 @SpringBootApplication
 public class DeliveryRestApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DeliveryRestApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryRestApiApplication.class, args);
+    }
 
 }
